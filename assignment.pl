810a8deea6546d14b431af644624f5181004b465
jamesor19 <31923110+jamesor19@users.noreply.github.com>
2017-11-16 21:24:23 +0000

    Partially completed

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,13 @@
 %Definitions for isElementInList(El, List)
+isElementInList(El,[El|_]).
+isElementInList(El,[_|T]):-
+    isElementInList(El,T).
 
 %Definitions for reverseList(List, ReversedList)
 
 %insertElementIntoListEnd(El, List, NewList)
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([],L2,L2).
+mergeLists([H|T],L2,[H|Merged]):-mergeLists(T,L2,Merged).
+
